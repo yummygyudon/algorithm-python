b1f9cb2475246a44df9b6635787b113061acfa3f
DongGyu Jung <86935274+yummygyudon@users.noreply.github.com>
2021-11-07 23:58:43 +0900

    문자열 길이&정수확인

--- Programmers/Lv1/김서방찾기.py
+++ Programmers/Lv1/김서방찾기.py
@@ -14,3 +14,7 @@ def solution(seoul):
     return f"김서방은 {seoul.index('Kim')}에 있다"
 
 print(solution(["Jane", "Kim"]))
+
+def findKim(seoul):
+    return "김서방은 {}에 있다".format(seoul.index('Kim'))
+
---
+++ Programmers/Lv1/문자열다루기Basic.py
@@ -0,0 +1,36 @@
+# 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+# 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+#
+# 제한 사항
+# s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+# 입출력 예
+# s	return
+# "a234"	false
+# "1234"	true
+
+
+# 문자열 길이 조건 없을 때 정확도 100
+# def solution(s):
+#     try :
+#         int(s)
+#     except :
+#         answer = False
+#     else :
+#         answer = True
+#     return answer
+
+def solution(s):
+    if (len(s) == 4 or len(s) == 6) and s.isdigit() :
+        answer = True
+    else :
+        answer = False
+    return answer
+
+def solution(s):
+    return (len(s) == 4 or len(s) == 6) and s.isdigit()
+
+
+def alpha_string46(s):
+    return s.isdigit() and len(s) in (4, 6)
+
+print(solution("1234"))
