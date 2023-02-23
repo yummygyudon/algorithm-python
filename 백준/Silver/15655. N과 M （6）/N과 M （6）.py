7f36fcb738175223fb4b0dab51b71ad21e60ca5b
DongGyu Jung <bang2brew@gmail.com>
2023-02-23 20:18:15 +0900

    [Silver III] Title: N과 M (6), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,22 @@
+from sys import stdin
+input = stdin.readline
+
+NUM_QUANTITY, TARGET_POS = map(int, input().split())
+
+NUM_RANGE = list(map(int, input().split()))
+NUM_RANGE.sort()
+CHECK = [False] * NUM_QUANTITY
+NUMS = [0] * TARGET_POS
+
+def dfs(pos, index) :
+    if pos == TARGET_POS :
+        print(*NUMS)
+        return
+
+    for i in range(index,NUM_QUANTITY) :
+        if not CHECK[i] :
+            NUMS[pos] = NUM_RANGE[i]
+            CHECK[i] = True
+            dfs(pos+1, i+1)
+            CHECK[i] = False
+dfs(0,0)
