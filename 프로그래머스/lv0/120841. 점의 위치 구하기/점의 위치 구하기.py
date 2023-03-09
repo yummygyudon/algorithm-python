b9733740cfa1e773e7507e166028112c2e3cbc02
DongGyu Jung <bang2brew@gmail.com>
2023-03-10 01:30:50 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,11 @@
+def solution(dot):
+    if dot[0] > 0 :
+        if dot[1] > 0 :
+            return 1
+        elif dot[1] < 0 :
+            return 4
+    else :
+        if dot[1] > 0 :
+            return 2
+        elif dot[1] < 0 :
+            return 3
